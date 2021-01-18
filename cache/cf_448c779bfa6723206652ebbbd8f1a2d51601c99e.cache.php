8630abecb015c6b4c0fc12cd2b98049560c46ba3
Marcos Richard <marcos.richard@hotmail.com>
2021-01-18 09:32:45 -0300

    First Commit

---
+++ cache/cf_448c779bfa6723206652ebbbd8f1a2d51601c99e.cache.php
@@ -0,0 +1,21 @@
+<?php if (!defined('IN_SCRIPT')) {die();} $hesk_settings['custom_fields']=array (
+  'custom1' => 
+  array (
+    'use' => '1',
+    'place' => '0',
+    'type' => 'text',
+    'req' => '2',
+    'category' => 
+    array (
+    ),
+    'name' => 'Setor',
+    'value' => 
+    array (
+      'max_length' => 255,
+      'default_value' => '',
+    ),
+    'order' => '10',
+    'title' => 'Setor',
+    'name:' => 'Setor:',
+  ),
+);
---
+++ cache/status_448c779bfa6723206652ebbbd8f1a2d51601c99e.cache.php
@@ -0,0 +1,32 @@
+<?php if (!defined('IN_SCRIPT')) {die();} $hesk_settings['statuses']=array (
+  0 => 
+  array (
+    'name' => 'Novo',
+    'class' => 'open',
+  ),
+  1 => 
+  array (
+    'name' => 'Aguardando resposta',
+    'class' => 'waitingreply',
+  ),
+  2 => 
+  array (
+    'name' => 'Respondido',
+    'class' => 'replied',
+  ),
+  3 => 
+  array (
+    'name' => 'resolvido',
+    'class' => 'resolved',
+  ),
+  4 => 
+  array (
+    'name' => 'Em progresso',
+    'class' => 'inprogress',
+  ),
+  5 => 
+  array (
+    'name' => 'Em espera',
+    'class' => 'onhold',
+  ),
+);
